d1891f97fc0bde2ff4d8da6c3d4c56ee126710e2
Mohammed Hayder <Mr.hamood277@gmail.com>
2022-05-05 21:21:48 +0300

    css fixes

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -40,7 +40,7 @@ const Search = () => {
   }
 
   return (
-    <form method='post' className='relative z-10 w-full' onSubmit={handleSearch}>
+    <form method='post' className='relative z-20 w-full' onSubmit={handleSearch}>
       <input
         type='search'
         id='search'
