c6a7adda9984ff017bd8ffa865515278e2314566
Mohammed Hayder <Mr.hamood277@gmail.com>
2022-07-08 20:22:19 +0300

    minor fixes

--- frontend/src/components/ImageUpload.js
+++ frontend/src/components/ImageUpload.js
@@ -25,7 +25,7 @@ const ImageUpload = ({ data }) => {
       {console.log(foodFileURLs)}
 
       <div
-        className={`flex flex-wrap justify-center gap-6 py-4 overflow-y-auto bg-gray-100 rounded-lg cursor-pointer dark:bg-gray-700 w-[30rem]`}
+        className={`grid grid-cols-3 gap-6 p-4 overflow-y-auto bg-gray-100 rounded-lg cursor-pointer dark:bg-gray-700 w-[30rem]`}
       >
         {foodFileURLs.length === 0 ? (
           <img
@@ -35,27 +35,34 @@ const ImageUpload = ({ data }) => {
             className='object-cover p-1 border border-gray-400 w-28 min-h-fit h-28 dark:border-gray-300 rounded-xl'
           />
         ) : (
-          foodFileURLs.map((fileURL, idx) => (
-            <div key={idx} className={`flex flex-col gap-y-3 max-h-44 h-44`}>
-              <img
-                loading='lazy'
-                src={fileURL}
-                alt={data?.foodName}
-                className='object-cover p-1 border border-gray-400 max-w-[7rem] min-h-fit h-28 dark:border-gray-300 rounded-xl'
-              />
-              {/* delete image button */}
-              <button
-                type='button'
-                className='py-2 text-white transition-colors bg-red-500 rounded-full hover:bg-red-700'
-                onClick={() => {
-                  setFoodFileURLs(foodFileURLs.filter(url => url !== fileURL))
-                }}
+          <>
+            {foodFileURLs.map((fileURL, idx) => (
+              <div
+                key={idx}
+                className={`flex items-center flex-col gap-y-3 max-h-44 h-44`}
               >
-                حذف
-              </button>
-            </div>
-          ))
+                <img
+                  loading='lazy'
+                  src={fileURL}
+                  alt={data?.foodName}
+                  className='object-cover p-1 border border-gray-400 max-w-[7rem] min-h-fit h-28 dark:border-gray-300 rounded-xl'
+                />
+                <button
+                  type='button'
+                  className='min-w-full py-2 text-white transition-colors bg-red-500 rounded-full hover:bg-red-700'
+                  onClick={() => {
+                    setFoodFileURLs(foodFileURLs.filter(url => url !== fileURL))
+                  }}
+                >
+                  حذف
+                </button>
+              </div>
+            ))}
+          </>
         )}
+        <h3 className='col-span-3 text-xl text-center text-green-600 dark:text-green-400'>
+          لا تنسى الضغط على زر تحديث أسفل الصفحة لتحميل الصور
+        </h3>
       </div>
 
       <input
