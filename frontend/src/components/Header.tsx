34ae6ec45a9fff3a3796e7f5d5936bc21d7580f1
Mohammed Hayder <Mr.hamood277@gmail.com>
2022-07-26 22:03:48 +0300

    changes

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,7 +4,6 @@ import useAxios from '../hooks/useAxios'
 import Search from './Search'
 import Nav from './Nav'
 import ScrollDown from './ScrollDown'
-import Logo from './Icons/Logo'
 
 const Header = () => {
   const [data, setData] = useState<{ appTagline: string }>()
@@ -35,11 +34,11 @@ const Header = () => {
         <div className='container mx-auto ltr'>
           {/* Search form and main hero */}
           <main className='flex flex-col items-center justify-center min-h-screen'>
-            <Logo
-              width='32 xl:w-48'
-              height='32 xl:h-48'
-              className='mb-20'
-              color='white'
+            {/* logo */}
+            <img
+              src='/assets/img/icons/logo.svg'
+              alt='logo'
+              className='w-auto h-48 mb-20'
             />
             <h1 className='max-w-xs px-2 mb-24 text-lg font-bold leading-loose text-center text-white select-none sm:max-w-fit xl:text-3xl sm:text-xl md:text-4xl rtl'>
               <span className='inline-flex h-20 pt-2 overflow-x-hidden animate-type sm:whitespace-nowrap'>
