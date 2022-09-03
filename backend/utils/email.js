5e40e4472f45cc816d4aad2cba162f5c0b5e72a6
Mohammed Hayder <mr.hamood277@gmail.com>
2022-09-03 16:24:13 +0300

    changes

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,5 +1,6 @@
 import nodemailer from 'nodemailer'
 import asyncHandler from 'express-async-handler'
+import { ADMIN_EMAIL } from '../data/constants.js'
 
 const email = asyncHandler(async ({ name, subject, from, to, msg }) => {
   /**
@@ -7,7 +8,7 @@ const email = asyncHandler(async ({ name, subject, from, to, msg }) => {
    */
 
   to = to || process.env.MAILER_EMAIL
-  from = from || process.env.MAILER_EMAIL
+  from = from || ADMIN_EMAIL?.userEmail
   name = name || to
 
   // create reusable transporter object using the default SMTP transport
